04329d856559e0d1a22494c90a42457e4ad10422
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-01-17 21:40:21 -0700

    added array for lower, upper,special,and number for password

--- pass.js
+++ pass.js
@@ -1,16 +1,24 @@
 // Assignment code here
-
+var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var password = {
+  minChar : 8,
+  maxChar: 20,
+
+  lowerCase: ["abcdefghijklmnopqrstuvxwyz"],
+  UpperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+  specialChar: [""],
+  num: ["0123456789"],
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(passwordText);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
